143ebcf03337e754912d50391552bd8631f26fe0
船长 <zhengliang166@126.com>
2022-11-22 12:29:47 +0800

    rabbitmq 测试类

---
+++ demo-springboot-rabbitmq/src/test/java/com/zhengl/rabbitmq/DemoSpringbootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengl.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringbootRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
