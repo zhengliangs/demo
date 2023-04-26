c2808a1d94092eb24c634a38700ca445a65562d3
船长 <zhengliang166@126.com>
2023-04-26 18:31:21 +0800

    cglib学习

---
+++ demo-spring/src/main/java/com/zhengl/spring/cglib/CGlibTest.java
@@ -0,0 +1,40 @@
+package com.zhengl.spring.cglib;
+
+import com.zhengl.spring.staticproxy.User;
+import com.zhengl.spring.staticproxy.UserService;
+import com.zhengl.spring.staticproxy.UserServiceImpl;
+import org.springframework.cglib.proxy.Enhancer;
+import org.springframework.cglib.proxy.MethodInterceptor;
+import org.springframework.cglib.proxy.MethodProxy;
+
+import java.lang.reflect.Method;
+
+public class CGlibTest {
+
+    public static void main(String[] args) {
+        // 1. 需要先创建原始对象
+        UserService userService = new UserServiceImpl();
+
+        /*
+         *
+         */
+        Enhancer enhancer = new Enhancer();
+        enhancer.setClassLoader(CGlibTest.class.getClassLoader());
+        enhancer.setSuperclass(userService.getClass());
+
+        MethodInterceptor interceptor = new MethodInterceptor() {
+            @Override
+            public Object intercept(Object o, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+
+                System.out.println(" ---cglib.log--- ");
+                Object ret = method.invoke(userService, args);
+                return ret;
+            }
+        };
+        enhancer.setCallback(interceptor);
+
+        UserService userServiceProxy = (UserService) enhancer.create();
+        userServiceProxy.login("cglib", "123456");
+        userServiceProxy.register(new User());
+    }
+}
